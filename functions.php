06ac5545b01684c86454958b5a6e2404da78023a
DesireeBesnard <desiree.besnard@gmail.com>
2020-12-29 14:44:57 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Include CSS files
+ */
+function theme_enqueue_scripts() {
+        wp_enqueue_style( 'Font_Awesome', 'https://use.fontawesome.com/releases/v5.11.2/css/all.css' );
+        wp_enqueue_style( 'Bootstrap_css', '/node_modules/mdbootstrap/css/bootstrap.min.css' );
+        wp_enqueue_style( 'MDB', '/node_modules/mdbootstrap/css/mdb.min.css' );
+        wp_enqueue_style( 'Style', get_template_directory_uri() . '/css/style.css' );
+        wp_enqueue_script( 'jQuery', '/node_modules/mdbootstrap/js/jquery.min.js' );
+        wp_enqueue_script( 'Tether', '/node_modules/mdbootstrap/js/popper.min.js' );
+        wp_enqueue_script( 'Bootstrap', '/node_modules/mdbootstrap/js/bootstrap.min.js' );
+        wp_enqueue_script( 'MDB', '/node_modules/mdbootstrap/js/mdb.min.js' );
+        wp_enqueue_script( 'Script', get_template_directory_uri() . '/js/script.js' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
+
+?>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Temple du bien-être</title>
+    <?php wp_head(); ?>
+</head>
+<body>
