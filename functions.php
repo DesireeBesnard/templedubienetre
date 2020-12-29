5ce0d94b4b1c63f12f69e05ff5937a53856c8a7f
DesireeBesnard <desiree.besnard@gmail.com>
2020-12-29 23:42:21 +0100

    ajout de la navbar + affichage des derniers articles sur la page blog + pagination dynamique

--- functions.php
+++ functions.php
@@ -16,4 +16,13 @@ function theme_enqueue_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
 
+/**
+ * Setup Theme
+ */
+ function templedubientretheme_setup() {
+        // Add featured image support
+        add_theme_support('post-thumbnails');
+    }
+    add_action('after_setup_theme', 'templedubientretheme_setup');
+
 ?>
--- index.php
+++ index.php
@@ -2,100 +2,25 @@
 
     <!--Main Navigation-->
     <header>
-
-        <!-- Navbar -->
-        <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
-            <div class="container">
-
-                <!-- Brand -->
-                <a class="navbar-brand pt-0 waves-effect" href="">
-                    <!-- <img src="https://mdbootstrap.com/img/logo/mdb-noshadow-38px" alt="MDB logo"> -->
-                </a>
-
-                <!-- Collapse -->
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <!-- Links -->
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    <!-- Left -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link waves-effect" href="#">Home
-                                <span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle waves-effect" id="navbarDropdownMenuLink1" data-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false">Categories </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="">Marketing Automation</a>
-                                <a class="dropdown-item" href="">Web Push Notifications</a>
-                                <a class="dropdown-item" href="">Analytics & Tag Manager</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect" href="https://mdbootstrap.com/docs/b4/jquery/getting-started/download/" target="_blank">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect" href="https://mdbootstrap.com/education/bootstrap/" target="_blank">Link</a>
-                        </li>
-                    </ul>
-
-                    <!-- Right -->
-                    <ul class="navbar-nav nav-flex-icons">
-                        <li class="nav-item">
-                            <a href="https://www.facebook.com/mdbootstrap" class="nav-link waves-effect" target="_blank">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="https://twitter.com/MDBootstrap" class="nav-link waves-effect" target="_blank">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="https://github.com/mdbootstrap/mdb-ui-kit" class="nav-link border border-light rounded waves-effect"
-                                target="_blank">
-                                <i class="fa fa-github mr-2"></i>MDB GitHub
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-
-            </div>
-        </nav>
-        <!-- Navbar -->
+        <?php require_once('components/navbar.inc.php'); ?>
 
         <!-- Intro -->
         <div class="card card-intro blue-gradient mb-4">
-
             <div class="card-body white-text rgba-black-light text-center pt-5 pb-4">
-
                 <!--Grid row-->
                 <div class="row d-flex justify-content-center">
-
                     <!--Grid column-->
                     <div class="col-md-6">
-
                         <h1 class="font-weight-bold mb-4">Website Name</h1>
                         <p class="lead mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad impedit corporis ratione facere?
                             Cupiditate unde aliquid reiciendis animi, quas inventore, praesentium neque voluptatem, iusto
                             perferendis placeat similique dolor eum?
                         </p>
-
                     </div>
                     <!--Grid column-->
-
                 </div>
                 <!--Grid row-->
-
             </div>
-
         </div>
         <!-- Intro -->
 
@@ -109,7 +34,6 @@
             <!--Section: Dynamic Content Wrapper-->
             <section>
               <div id="dynamic-content"></div>
-
             </section>
             <!--Section: Dynamic Content Wrapper-->
 
@@ -119,185 +43,58 @@
                 <!--Section heading-->
                 <h1 class="h2 font-weight-bold my-4">Recent articles</h1>
 
-                <!--Grid row-->
-                <div class="row wow fadeIn">
+                    <!--Grid row-->
+                    <div class="row wow fadeIn">
+<?php
 
-                    <!--Grid column-->
-                    <div class="col-lg-4 col-md-12 mb-4">
-                        <!--Featured image-->
-                        <div class="view overlay hm-white-slight rounded z-depth-2 mb-4">
-                            <img src="https://mdbootstrap.com/img/Photos/Others/images/81.jpg" class="img-fluid" alt="First sample image">
-                            <a>
-                                <div class="mask"></div>
+if ( have_posts() ) { // cette condition vérifie s'il y a des messages existants à afficher.
+    $counter = 1;
+    while ( have_posts() ) { // début de la boucle WordPress. Parcours les articles et exécute le code entre les crochets {} pour chaque article.
+        the_post(); // cette fonction WP récupère le post suivant, cela nous permet d'utiliser les fonctions ci-dessous 
+?>
+                        <!--Grid column-->
+                        <div class="col-lg-4 col-md-12 mb-4">
+                            <!--Featured image-->
+                            <div class="view overlay hm-white-slight rounded z-depth-2 mb-4">
+                                <?php the_post_thumbnail( 'medium-large', array( 'class'=> 'img-fluid')); ?>
+                                <a href="<?php echo get_permalink() ?>">
+                                    <div class="mask"></div>
+                                </a>
+                            </div>
+
+                            <!--Excerpt-->
+                            <a href="" class="pink-text">
+                                <h6 class="mb-3 mt-4">
+                                    <i class="fa fa-bolt"></i>
+                                    <strong> <?php the_category(', '); ?></strong>
+                                </h6>
                             </a>
+                            <h4 class="mb-3 font-weight-bold dark-grey-text">
+                                <strong><?php the_title(); ?></strong>
+                            </h4>
+                            <p>by
+                                <a href="<?php echo get_permalink() ?>" class="font-weight-bold dark-grey-text"><?php echo get_the_author(); ?></a>, <?php echo get_the_date(); ?></p>
+                            <p class="grey-text"><?php the_excerpt(); ?></p>
+                            <a href="<?php echo get_permalink() ?>" class="btn btn-info btn-rounded btn-md">Read more</a>
                         </div>
+                        <!--Grid column-->
 
-                        <!--Excerpt-->
-                        <a href="" class="pink-text">
-                            <h6 class="mb-3 mt-4">
-                                <i class="fa fa-bolt"></i>
-                                <strong> Web Push Notifications</strong>
-                            </h6>
-                        </a>
-                        <h4 class="mb-3 font-weight-bold dark-grey-text">
-                            <strong>This is title of the news</strong>
-                        </h4>
-                        <p>by
-                            <a class="font-weight-bold dark-grey-text">Billy Forester</a>, 15/07/2018</p>
-                        <p class="grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
-                            placeat facere possimus voluptas.</p>
-                        <a class="btn btn-info btn-rounded btn-md">Read more</a>
+<?php 
+        if ($counter % 3 == 0) {
+?>
                     </div>
-                    <!--Grid column-->
+                    <!--Grid row-->
 
-                    <!--Grid column-->
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <!--Featured image-->
-                        <div class="view overlay hm-white-slight rounded z-depth-2 mb-4">
-                            <img src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" class="img-fluid" alt="Second sample image">
-                            <a>
-                                <div class="mask"></div>
-                            </a>
-                        </div>
-
-                        <!--Excerpt-->
-                        <a href="" class="deep-orange-text">
-                            <h6 class="mb-3 mt-4">
-                                <i class="fa fa-pie-chart"></i>
-                                <strong> Analytics</strong>
-                            </h6>
-                        </a>
-                        <h4 class="mb-3 font-weight-bold dark-grey-text">
-                            <strong>This is title of the news</strong>
-                        </h4>
-                        <p>by
-                            <a class="font-weight-bold dark-grey-text">Billy Forester</a>, 15/07/2018</p>
-                        <p class="grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
-                            placeat facere possimus voluptas.</p>
-                        <a class="btn btn-info btn-rounded btn-md">Read more</a>
+                    <!--Grid dynamic row-->
+                    <div class="row wow fadeIn">
+<?php
+        }
+        $counter++;
+    } // end while
+} // end if
+?>
                     </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <!--Featured image-->
-                        <div class="view overlay hm-white-slight rounded z-depth-2 mb-4">
-                            <img src="https://mdbootstrap.com/img/Photos/Others/images/13.jpg" class="img-fluid" alt="Thrid sample image">
-                            <a>
-                                <div class="mask"></div>
-                            </a>
-                        </div>
-
-                        <!--Excerpt-->
-                        <a href="" class="cyan-text">
-                            <h6 class="mb-3 mt-4">
-                                <i class="fa fa-cogs"></i>
-                                <strong> Marketing Automation</strong>
-                            </h6>
-                        </a>
-                        <h4 class="mb-3 font-weight-bold dark-grey-text">
-                            <strong>This is title of the news</strong>
-                        </h4>
-                        <p>by
-                            <a class="font-weight-bold dark-grey-text">Billy Forester</a>, 15/07/2018</p>
-                        <p class="grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
-                            placeat facere possimus voluptas.</p>
-                        <a class="btn btn-info btn-rounded btn-md">Read more</a>
-                    </div>
-                    <!--Grid column-->
-
-                </div>
-                <!--Grid row-->
-
-                <!--Grid row-->
-                <div class="row wow fadeIn">
-
-                    <!--Grid column-->
-                    <div class="col-lg-4 col-md-12 mb-2">
-                        <!--Featured image-->
-                        <div class="view overlay hm-white-slight rounded z-depth-2 mb-4">
-                            <img src="https://mdbootstrap.com/img/Photos/Others/images/81.jpg" class="img-fluid" alt="First sample image">
-                            <a>
-                                <div class="mask"></div>
-                            </a>
-                        </div>
-
-                        <!--Excerpt-->
-                        <a href="" class="pink-text">
-                            <h6 class="mb-3 mt-4">
-                                <i class="fa fa-bolt"></i>
-                                <strong> Web Push Notifications</strong>
-                            </h6>
-                        </a>
-                        <h4 class="mb-3 font-weight-bold dark-grey-text">
-                            <strong>This is title of the news</strong>
-                        </h4>
-                        <p>by
-                            <a class="font-weight-bold dark-grey-text">Billy Forester</a>, 15/07/2018</p>
-                        <p class="grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
-                            placeat facere possimus voluptas.</p>
-                        <a class="btn btn-info btn-rounded btn-md">Read more</a>
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-lg-4 col-md-6 mb-2">
-                        <!--Featured image-->
-                        <div class="view overlay hm-white-slight rounded z-depth-2 mb-4">
-                            <img src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" class="img-fluid" alt="Second sample image">
-                            <a>
-                                <div class="mask"></div>
-                            </a>
-                        </div>
-
-                        <!--Excerpt-->
-                        <a href="" class="deep-orange-text">
-                            <h6 class="mb-3 mt-4">
-                                <i class="fa fa-pie-chart"></i>
-                                <strong> Analytics</strong>
-                            </h6>
-                        </a>
-                        <h4 class="mb-3 font-weight-bold dark-grey-text">
-                            <strong>This is title of the news</strong>
-                        </h4>
-                        <p>by
-                            <a class="font-weight-bold dark-grey-text">Billy Forester</a>, 15/07/2018</p>
-                        <p class="grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
-                            placeat facere possimus voluptas.</p>
-                        <a class="btn btn-info btn-rounded btn-md">Read more</a>
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-lg-4 col-md-6 mb-2">
-                        <!--Featured image-->
-                        <div class="view overlay hm-white-slight rounded z-depth-2 mb-4">
-                            <img src="https://mdbootstrap.com/img/Photos/Others/images/13.jpg" class="img-fluid" alt="Thrid sample image">
-                            <a>
-                                <div class="mask"></div>
-                            </a>
-                        </div>
-
-                        <!--Excerpt-->
-                        <a href="" class="cyan-text">
-                            <h6 class="mb-3 mt-4">
-                                <i class="fa fa-cogs"></i>
-                                <strong> Marketing Automation</strong>
-                            </h6>
-                        </a>
-                        <h4 class="mb-3 font-weight-bold dark-grey-text">
-                            <strong>This is title of the news</strong>
-                        </h4>
-                        <p>by
-                            <a class="font-weight-bold dark-grey-text">Billy Forester</a>, 15/07/2018</p>
-                        <p class="grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
-                            placeat facere possimus voluptas.</p>
-                        <a class="btn btn-info btn-rounded btn-md">Read more</a>
-                    </div>
-                    <!--Grid column-->
-
-                </div>
-                <!--Grid row-->
+                    <!--Grid row-->
 
                 <!--Pagination -->
                 <nav class="d-flex justify-content-center my-4 wow fadeIn">
@@ -415,3 +212,15 @@
         </div>
         <!--/.Copyright-->
 <?php  get_footer(); ?>
+
+<?php
+    // if ( have_posts() ) {
+    //     $counter = 1;
+    //     while ( have_posts() ) {
+    //         the_post();
+    //         if ($counter % 3 == 0) { 
+    //         }
+    //         $counter++;
+    //     } // end while
+    // } // end if
+?>
