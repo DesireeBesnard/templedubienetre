55e62b57b7348d2c34087f64a98a60ba69e2dd18
DesireeBesnard <desiree.besnard@gmail.com>
2021-01-12 00:32:17 +0100

    primary navbar, pages added, slider and gift coupons downloaded

--- functions.php
+++ functions.php
@@ -31,6 +31,18 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 /**
+ * Register Custom Bootstrap Navigation Walker
+ */
+ function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+
+register_nav_menus( array(
+  'primary' => __( 'Primary Menu', 'THEMENAME' ),
+) );
+
+/**
  * Setup Theme
  */
  function mdbtheme_setup() {
